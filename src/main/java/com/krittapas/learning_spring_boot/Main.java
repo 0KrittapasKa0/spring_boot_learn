35aacc46dbdd74862cf6226c2c4fc264d367a8f7
0KrittapasKa0 <krittapas.ka@outlook.com>
2024-12-30 15:35:09 +0700

    add entity(Model) and config properties

--- src/main/java/com/krittapas/learning_spring_boot/Main.java
+++ src/main/java/com/krittapas/learning_spring_boot/Main.java
@@ -1,7 +1,9 @@
 package com.krittapas.learning_spring_boot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
@@ -9,5 +11,11 @@ public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner->{
+			System.out.println("Hello World!");
+		};
+	}
 }
---
+++ src/main/java/com/krittapas/learning_spring_boot/entity/Person.java
@@ -0,0 +1,61 @@
+package com.krittapas.learning_spring_boot.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String fName;
+
+    @Column(name = "last_name")
+    private String LName;
+
+    public Person() {
+
+    }
+
+    public Person(String fName, String LName) {
+        this.fName = fName;
+        this.LName = LName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getLName() {
+        return LName;
+    }
+
+    public void setLName(String LName) {
+        this.LName = LName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [id=" + id + ", fName=" + fName + ", LName=" + LName + "]";
+    }
+}
